2262ba8188c0031f68d516467132b818f8c8e66a
Noftyfootie <owolabinofisat7@gmail.com>
2026-02-11 22:33:34 +0100

    UPDATE

--- starter/script.js
+++ starter/script.js
@@ -503,3 +503,17 @@ const totalDepositsUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
 */
+/*
+const eurToUsd = 1.1;
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+*/
